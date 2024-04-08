e4e88ce1f02c80c9e37225406d8c33b2e5e1990c
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-04-08 04:09:06 +0200

    hbnb 3

---
+++ web_dynamic/3-hbnb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module is used for getting the html page"""
+from flask import Flask, render_template
+import uuid
+from models import storage
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
+
+
+@app.route('/3-hbnb')
+def display_hbnb():
+    """Generating page with menu of states/cities"""
+    the_states = storage.all('State')
+    the_amenities = storage.all('Amenity')
+    the_places = storage.all('Place')
+    the_cache_id = uuid.uuid4()
+    return render_template('3-hbnb.html',
+                           states=the_states,
+                           amenities=the_amenities,
+                           places=the_places,
+                           cache_id=the_cache_id)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Closing the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
